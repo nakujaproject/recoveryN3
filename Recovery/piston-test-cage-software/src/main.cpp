6801f657c1f1dd7b7fae4196152770ba16c7ed16
bytecod31 <emwiti658@gmail.com>
2023-04-13 21:18:20 +0300

    added piston test system flowchart

--- Recovery/piston-test-cage-software/src/main.cpp
+++ Recovery/piston-test-cage-software/src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
+#include <esp_now.h>
 #include "soc/rtc.h"
 #include "defs.h"
 #include "HX711.h"
 
-// load cell pins
+/* load cell pins  */
 const int load_cell_dt = 25;
 const int load_cell_sck = 26;
 
@@ -46,8 +47,49 @@ void callibrate_load_cell(){
 
   delay(1000);
 }
-
 /* End of load cell functions */
+
+
+  Serial.println("Initializing the scale");
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());      // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
+            // by the SCALE parameter (not set yet)
+            
+  scale.set_scale(INSERT YOUR CALIBRATION FACTOR);
+  //scale.set_scale(-471.497);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();               // reset the scale to 0
+
+  Serial.println("After setting up the scale:");
+
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());                 // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+            // by the SCALE parameter set with set_scale
+
+  Serial.println("Readings:");
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -56,7 +98,6 @@ void setup() {
 }
 
 void loop() {
-
   callibrate_load_cell();
   
 }
