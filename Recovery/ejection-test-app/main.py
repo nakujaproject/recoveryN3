a5f80a5d9d3e24e64ed0d6e85291be4052bf96b4
bytecod31 <emwiti658@gmail.com>
2023-04-19 03:17:17 +0300

    improved UI

--- Recovery/ejection-test-app/main.py
+++ Recovery/ejection-test-app/main.py
@@ -20,20 +20,44 @@ class App:
     def __init__(self,parent):
         super().__init__()
         self.parent = parent
-        self.input_parameters = Frame(parent, bg="gray40")
-        self.logo = Frame(parent, bg="gray40")
+
+        self.parameters_frame = Frame(self.parent, bg="gray30")
+        self.parameters_frame.grid(row=1, column=0, padx=5)
+        label = Label(text="Control Parameters", fg="white", bg="gray30", borderwidth=4, font=("Arial", 14, "bold"))
+        fixed_label = Label(text="Fixed Parameters", fg="white", bg="gray30", borderwidth=4, font=("Arial", 14, "bold"))
+        self.input_parameters = LabelFrame(self.parameters_frame, labelwidget=label, bg="gray30")
+        self.input_parameters.grid(row=1, column=0, sticky='ew', pady=15, ipady=10)
+
+        self.fixed_parameters = LabelFrame(self.parameters_frame, labelwidget=fixed_label, bg="gray30")
+        self.fixed_parameters.grid(row=2, column=0, sticky='ew', pady=15, ipady=10)
+
+        message_label= Label(text="Status Messages", fg="white", bg="gray30", borderwidth=4, font=("Arial", 14, "bold"))
+        self.message_frame = LabelFrame(self.parameters_frame, labelwidget=message_label, bg="gray30")
+        self.message_frame.grid(row=3, column=0, sticky='ew')
+
+        self.logo = Frame(parent, bg="gray30")
         self.graph_tab = Frame(parent, padx=10, bg="gray40")
+        self.message_variable = StringVar()
+        self.message_entry = Entry(self.message_frame, font=("Arial", 15, "bold"), fg="darkgoldenrod1", bg="gray30", borderwidth=3, width=44, textvariable=self.message_variable)
+        self.message_entry.grid(row=0, column=0, ipadx=4, ipady=4, sticky='ew')
 
         # test data
         style.use('ggplot')
-        self.fig = plt.figure(figsize=(8, 6), dpi=100, edgecolor="green", facecolor="gray")
+        self.fig = plt.figure(figsize=(8, 6), dpi=100, edgecolor="green", facecolor="dimgray")
         self.ax = self.fig.add_subplot(1, 1, 1)
+        self.ax.spines['bottom'].set_color('black')
+        self.ax.spines['top'].set_color('black')
+        self.ax.xaxis.label.set_color('white')
+        self.ax.yaxis.label.set_color('white')
+        self.ax.tick_params(axis='x', colors='white')
+        self.ax.tick_params(axis='y', colors='white')
+
 
         self.xs = []
         self.ys = []
+        self.piston_force = 0
 
         self.logo.grid(row=0, column=0)
-        self.input_parameters.grid(row=1, column=0)
         self.graph_tab.grid(row=0, column=1, columnspan=4, sticky="w")
 
         # mqtt client creation
@@ -45,10 +69,19 @@ class App:
         # set default appearance configurations
 
         self.fire_button_config = {
-            "fg": "red",
+            "fg": "green2",
             "bg": "gray10",
-            "font": ("Arial", 15, "bold"),
-            "relief": RAISED,
+            "font": ("Arial", 17, "bold"),
+            "relief": RIDGE,
+            "padx": 5,
+            "pady": 4,
+        }
+
+        self.abort_button_config = {
+            "fg": "black",
+            "bg": "red",
+            "font": ("Arial", 17, "bold"),
+            "relief": RIDGE,
             "padx": 5,
             "pady": 4,
         }
@@ -56,10 +89,10 @@ class App:
         self.standby_button_config = {
             "fg": "orange",
             "bg": "gray10",
-            "font": ("Arial", 15, "bold"),
+            "font": ("Arial", 13, "bold"),
             "relief": RAISED,
-            "padx": 5,
-            "pady": 4,
+            "padx": 2,
+            "pady": 2,
         }
 
         self.btn_config = {
@@ -74,23 +107,25 @@ class App:
         self.serial_button_config ={
             "fg": "white",
             "bg": "steelblue",
-            "font": ("Arial", 12, "bold"),
+            "font": ("Arial", 13, "bold"),
             "relief": RAISED
         }
 
         self.entry_config ={
-            "fg": "black",
-            "bg": "white",
-            "font": ("Arial", 12, "normal"),
-            "relief": RAISED,
-            "width": 20
+            "fg": "red",
+            "bg": "black",
+            "font": ("Arial", 14, "bold"),
+            "relief": SUNKEN,
+            "width": 20,
+            "borderwidth": 4,
+            "insertbackground": "#ffffff"
         }
 
         self.label_config = {
             "font": ("Arial", 12, "normal"),
             "pady": 5,
-            "bg" : "gray40",
-            "fg": "steelblue1"
+            "bg" : "gray30",
+            "fg": "white"
         }
 
         self.radio_button_config = {
@@ -122,75 +157,84 @@ class App:
         # Nakuja project logo
         logo = ImageTk.PhotoImage(Image.open("app-images/logo.png"))
 
-        logo_label = Label(self.logo, image=logo, bg="gray40")
+        logo_label = Label(self.logo, image=logo, bg="gray30", width=40, height=40)
         logo_label.image = logo
         logo_label.grid(row=0, column=0)
-        app_title = Label(self.logo, text="Piston Ejection Test System", font=("arial", 15, "bold"), bg="gray40", fg="white")
-        app_title.grid(row=1, column=0)
+        app_title = Label(self.logo, text="Piston Ejection Test Control", font=("arial", 13, "bold"), bg="gray30", fg="white")
+        app_title.grid(row=0, column=1)
 
         ports = self.get_serial_ports()
         # create and pack widgets for port
-        self.port_select_label = Label(self.input_parameters, self.label_config, text="Select port:")
+        style = ttk.Style()
+        style.theme_use('clam')
+        style.configure("TCombobox", fieldbackground="black", fieldforeground="red", background="black", font=("Arial", 14, "bold"), borderwidth=4, width=27)
+        self.port_select_label = Label(self.input_parameters, self.label_config, text="SELECT PORT:")
         # self.port_select_entry = Entry(self.input_parameters, self.entry_config, justify="right")
-        self.port_select_combobox = ttk.Combobox(self.input_parameters, width=27, values=ports)
+        self.port_select_combobox = ttk.Combobox(self.input_parameters, width=19, values=ports, font=("Arial", 14, "bold"))
 
         # button to allow serial connection
-        self.establish_serial_connection_button = Button(self.input_parameters, self.serial_button_config, text="Check connection", command=self.test_serial_connection)
+        self.establish_serial_connection_button = Button(self.input_parameters, self.serial_button_config, text="CHECK", command=self.test_serial_connection)
         self.establish_serial_connection_button.grid(padx=4)
 
         # create and pack widgets for simulation time selection
-        self.firing_time_label = Label(self.input_parameters, self.label_config, text="Pin HIGH time:")
+        self.firing_time_label = Label(self.input_parameters, self.label_config, text="PIN HIGH TIME(S):")
         self.firing_time_entry = Entry(self.input_parameters, self.entry_config, justify="right")
 
-        # widget to show type of powder being used
-        self.powder_type_label = Label(self.input_parameters, self.label_config, text="Ejection powder:")
-        self.powder_type_entry = Entry(self.input_parameters, self.entry_config, justify="right")
-        self.powder_type_entry.insert(0, "Crimson Powder")
-        self.powder_type_entry.config(state=DISABLED)
-
         # widget to display the mass of crimson powder used
-        self.mass_of_crimson_used_label = Label(self.input_parameters, self.label_config, text="Mass of powder used:")
+        self.mass_of_crimson_used_label = Label(self.input_parameters, self.label_config, text="POWDER MASS(G):")
         self.mass_of_crimson_used_entry = Entry(self.input_parameters, self.entry_config, justify="right")
 
-        # widget to show date and time of ejection test
-        self.test_date_label = Label(self.input_parameters, self.label_config, text="Date:")
-        self.test_date_entry = Entry(self.input_parameters, self.entry_config, justify="right")
-        self.test_date_entry.insert(0, self.todays_date)
-        self.test_date_entry.config(state=DISABLED)
 
+        # widget to show type of powder being used
+        self.powder_type_label = Label(self.fixed_parameters, self.label_config, text="POWDER TYPE:")
+        self.powder_type_entry = Entry(self.fixed_parameters, self.entry_config, justify="right")
+        self.powder_type_entry.insert(0, "CRIMSON")
+        # self.powder_type_entry.config(state=DISABLED)
+
+        # widget to show date and time of ejection test
+        self.test_date_label = Label(self.fixed_parameters, self.label_config, text="DATE:")
+        self.test_date_entry = Entry(self.fixed_parameters, self.entry_config, justify="right")
+        self.test_date_entry.insert(0, self.todays_date)
+        # self.test_date_entry.config(state=DISABLED)
 
         # widget to show test time
-        self.test_time_label = Label(self.input_parameters, self.label_config, text="Time:")
-        self.test_time_entry = Entry(self.input_parameters, self.entry_config, justify="right")
+        self.test_time_label = Label(self.fixed_parameters, self.label_config, text="TIME:")
+        self.test_time_entry = Entry(self.fixed_parameters, self.entry_config, justify="right")
         self.test_time_entry.insert(0, self.current_time)
-        self.test_time_entry.config(state=DISABLED)
+        # self.test_time_entry.config(state=DISABLED)
 
-        self.save_parameters = Button(self.input_parameters, self.btn_config, text="Save parameters", command=self.save_parameters_to_file)
-        self.save_parameters.grid(row=7, column=1)
+        self.save_parameters = Button(self.fixed_parameters, self.serial_button_config, text="SAVE", command=self.save_parameters_to_file)
+        self.save_parameters.grid(row=7, column=1, pady=6)
 
         # create a FIRE button
-        # play icon
-        play_icon = ImageTk.PhotoImage(Image.open("app-images/play.jpg"))
-        # play_icon_image = play_icon.subsample(1, 2) # resize to fit
 
-        self.control_buttons= Frame(self.parent, bg="gray40")
-        self.control_buttons.grid(row=0, column=1, padx=5, pady=5)
+        self.control_buttons= Frame(self.parent, bg="gray30")
+        self.control_buttons.grid(row=0, column=1, padx=5, pady=5, sticky='ew')
 
-        self.mqtt_label = Label(self.control_buttons, self.label_config, text="MQTT Broker URL: ")
+        mqttlabel = Label(text="Network", fg="white", bg="gray30", borderwidth=4, font=("Arial", 14, "bold"))
+        self.mqtt_frame = LabelFrame(self.control_buttons, labelwidget=mqttlabel, bg="gray30")
+        self.mqtt_frame.grid(row=0, column=0)
+
+        # buttons_label = Label(text="Network", fg="white", bg="gray30", borderwidth=4, font=("Arial", 14, "bold"))
+        self.btns_frame = Frame(self.control_buttons, bg="gray30")
+        self.btns_frame.grid(row=1, column=0, pady=10, sticky='ew', ipadx=5)
+
+        self.mqtt_label = Label(self.mqtt_frame, self.label_config, text="BROKER URL: ")
         self.mqtt_label.grid(row=0, column=1)
-        self.mqtt_url = Entry(self.control_buttons, self.entry_config)
+        self.mqtt_url = Entry(self.mqtt_frame, self.entry_config)
         self.mqtt_url.grid(row=0, column=2)
 
         self.status_variable = StringVar()
-        self.status_variable.set("Standby")
-        self.status_bar = Label(self.control_buttons, bg="gray40", fg="white", font=("arial", 11), border=2, textvariable=self.status_variable)
-        self.status_bar.grid(row=1, column=1, columnspan=6)
+        self.status_variable.set("STANDBY")
 
-        self.standby_button = Button(self.control_buttons, self.standby_button_config, text="STANDBY", command=self.connect_to_server)
-        self.standby_button.grid(row=0, column=3, padx=40)
+        self.standby_button = Button(self.mqtt_frame, self.standby_button_config, text="CONNECT", command=self.connect_to_server)
+        self.standby_button.grid(row=0, column=3, padx=40, pady=4)
 
-        self.fire_button = Button(self.control_buttons, self.fire_button_config, text="FIRE", command=self.send_fire_command)
+        self.fire_button = Button(self.btns_frame, self.fire_button_config, text="FIRE", command=self.send_fire_command)
         self.fire_button.grid(row=0, column=4)
+
+        self.abort_button = Button(self.btns_frame, self.abort_button_config, text="ABRT", command=self.send_abort_command)
+        self.abort_button.grid(row=0, column=5, padx=20, pady=4)
 
         # heating time progress bar
         # self.progress_bar = Frame(self.parent)
@@ -203,21 +247,23 @@ class App:
         # self.heating_value.grid(row=1, column=1)
 
         # arrange all the widgets on the screen
-        self.port_select_label.grid(row=0, column=0)
-        self.firing_time_label.grid(row=1, column=0)
-        self.powder_type_label.grid(row=2, column=0)
-        self.mass_of_crimson_used_label.grid(row=3, column=0)
-        self.test_date_label.grid(row=4, column=0)
-        self.test_time_label.grid(row=5, column=0)
+        self.port_select_label.grid(row=0, column=0, sticky='e')
+        self.firing_time_label.grid(row=1, column=0, sticky='e')
+        self.mass_of_crimson_used_label.grid(row=2, column=0, sticky='e')
+
+        self.powder_type_label.grid(row=0, column=0, sticky='e')
+        self.test_date_label.grid(row=1, column=0, sticky='e')
+        self.test_time_label.grid(row=2, column=0, sticky='e')
 
         # self.port_select_entry.grid(row=0, column=1, sticky="e")
         self.port_select_combobox.grid(row=0, column=1, sticky="e")
         self.establish_serial_connection_button.grid(row=0, column=2)
         self.firing_time_entry.grid(row=1, column=1, sticky="e")
-        self.powder_type_entry.grid(row=2, column=1, sticky="e")
-        self.mass_of_crimson_used_entry.grid(row=3, column=1, sticky="e")
-        self.test_date_entry.grid(row=4, column=1, sticky="e")
-        self.test_time_entry.grid(row=5, column=1, sticky="e")
+        self.mass_of_crimson_used_entry.grid(row=2, column=1, sticky="e")
+
+        self.powder_type_entry.grid(row=0, column=1, sticky="e")
+        self.test_date_entry.grid(row=1, column=1, sticky="e")
+        self.test_time_entry.grid(row=2, column=1, sticky="e")
 
     def get_serial_ports(self):
         """
@@ -332,22 +378,21 @@ class App:
         self.xs.append(datetime.now().strftime("%S"))
 
         # data to plot
-        self.ys.append(randint(10, 40))
+        self.ys.append(self.piston_force)
 
         self.xs = self.xs[-20:]
         self.ys = self.ys[-20:]
         
         self.ax.clear()
-        self.ax.plot(self.xs, self.ys)
+        self.ax.plot(self.xs, self.ys, color="g")
 
         # format plot
         plt.xticks(rotation=45, ha="right")
         plt.subplots_adjust(bottom=0.30)
-        plt.title("Force vs Time")
+        plt.title("Force vs Time", color="white")
         plt.ylabel("Force(N)")
         plt.xlabel("Time(Sec)")
 
-        # limit x axis to 20 items
 
     def connect_to_server(self):
         '''
@@ -359,10 +404,10 @@ class App:
 
         self.status_variable.set("Connecting to broker...")
 
-        self.client.connect(broker) # connect to broker
+        self.client.connect(broker)  # connect to broker
 
         # update standby button to ready
-        self.standby_button.config(fg="green1", text="READY")
+        self.standby_button.config(fg="green1", text="CONNECTED")
         self.client.subscribe(self.subscribe_topic)
 
         self.status_variable.set("Connected to broker")
@@ -374,26 +419,34 @@ class App:
         process any incoming data from MQTT server
         '''
 
-        print(str(message.payload.decode("utf-8")))
+        self.piston_force = message.payload.decode("utf-8")
 
         
     def send_fire_command(self):
         '''
-        semd command to fire the piston
+        send command to fire the piston
         :return:
         '''
 
         self.client.publish(self.publish_topic, "FIRE")
         self.client.loop_start()
 
+    def send_abort_command(self):
+        '''
+        send command to cancel piston firing
+        :return:
+        '''
+
+        self.client.publish(self.publish_topic, "ABORT")
+        self.client.loop_start()
 
 
 def run_app():
     root = Tk()
-    root.geometry('1250x700')
+    root.geometry('1250x740')
     root.title("N3 Ejection Test")
     root.resizable(False, False)
-    root.config(bg="gray40")
+    root.config(bg="gray30")
     main = App(root)
     # main.client.loop_start()
 
